4245ba3519a0a0111c94ba946b699f27b3d2a38f
WahyuPutraS <wahyusuartama2002@gmail.com>
2023-07-26 11:05:26 +0800

    Latihan

---
+++ Project1/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Program
+{
+
+    class Mahasiswa
+    {
+
+        public string Nama;
+
+
+
+        public void Named()
+        {
+            Nama = "Putu Wahyu Putra Suartama";
+
+           
+
+        }
+
+
+
+
+    }
+
+    class Identity
+    {
+        static void Main(string[] args)
+        {
+            var Maha = new Mahasiswa();
+            Maha.Named();
+            Console.WriteLine(Maha.Nama);
+
+        }
+
+    }
+
+
+
+
+
+
+
+}
+    
+
+  
+
+    
+    
+
